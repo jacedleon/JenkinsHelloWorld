e67023030a823e6d0110b6894a46d27807c15cd8
Jorge Acevedo <acevedodeleonjorge@gmail.com>
2022-11-02 12:13:20 +0100

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello this is for the jenkins changes test!!!"); 
+        System.out.println("Hello this is for the jenkins changes webhook test!!!"); 
     }
 }
